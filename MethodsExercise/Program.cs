72b69e132429a9a6e3690fe994a1e18935091c98
Ryan Gainor <rmgainor@gmail.com>
2023-05-12 15:38:31 -0500

    fixed the git problems

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Story();
+            var sum = AddNums(1, 2);
+            Console.WriteLine(sum);
+            subtractNums(5, 2);
+            multiplyNums(2, 2);
+            divideNums(4, 2);
+            modulusNums(5, 3);
+
+            Console.ReadKey();
+        }
+
+        public static void Story()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favoriteBand = Console.ReadLine();
+            Console.WriteLine($"Hello, my name is {name}. My favorite color is {favoriteColor}. My favorite animal is {favoriteAnimal}. My favorite band is {favoriteBand}.");
+        }
+
+        public static int AddNums(int num1, int num2)
+        {
+            int addNums = num1 + num2;
+            return addNums;          
+        }
+
+        public static int subtractNums(int num1, int num2)
+        {
+            int subtractNums = num1 - num2;
+            return subtractNums;
+        }
+
+        public static int multiplyNums(int num1, int num2)
+        {
+            int multiplyNums = num1 * num2;
+            return multiplyNums;
+        }
+
+        public static int divideNums(int num1, int num2)
+        {
+            int divideNums = num1 / num2;
+            return divideNums;
+        }
+
+        public static int modulusNums(int num1, int num2)
+        {
+            int modulusNums = num1 / num2;
+            return modulusNums;
         }
     }
 }
+
+    
+
