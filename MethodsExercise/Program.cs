c634d5244e7abbdd740e7e06da3ec03b29ffcecc
Ryan Gainor <rmgainor@gmail.com>
2023-07-05 23:30:47 -0500

    exercise completed

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,22 +4,27 @@
     {
         static void Main(string[] args)
         {
-            //Story();
-            var sum = AddNums(1, 2);
-            Console.WriteLine(sum);
-            subtractNums(5, 2);
-            multiplyNums(2, 2);
-            divideNums(4, 2);
-            modulusNums(5, 3);
+            Story();
 
-            Console.ReadKey();
+            // Store and use => int onePlusTwo = Add(1, 2);
+
+            // Just use it:
+
+            Console.WriteLine($"Add: {Add(3, 3)}");
+            Console.WriteLine($"Subtract: {Subtract(5, 2)}");
+            Console.WriteLine($"Multiply: {Multiply(2, 2)}");
+            Console.WriteLine($"Divide: {Divide(4, 2)}");
+
+            Console.WriteLine($"Params Keyword Method: {AddAllNums(1, 1, 1, 1, 1)}");
+
+
         }
 
         public static void Story()
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Console.WriteLine("What is your favorite color?");
+            Console.WriteLine($"Hello, {name}. What is your favorite color?");
             string favoriteColor = Console.ReadLine();
             Console.WriteLine("What is your favorite animal?");
             string favoriteAnimal = Console.ReadLine();
@@ -28,34 +33,44 @@
             Console.WriteLine($"Hello, my name is {name}. My favorite color is {favoriteColor}. My favorite animal is {favoriteAnimal}. My favorite band is {favoriteBand}.");
         }
 
-        public static int AddNums(int num1, int num2)
+        
+
+        public static int Add(int num1, int num2)
         {
-            int addNums = num1 + num2;
-            return addNums;          
+        return num1 + num2;         
         }
 
-        public static int subtractNums(int num1, int num2)
+        public static int Subtract(int num1, int num2)
         {
-            int subtractNums = num1 - num2;
-            return subtractNums;
+            return num1 - num2;
         }
 
-        public static int multiplyNums(int num1, int num2)
+        public static int Multiply(int num1, int num2)
         {
-            int multiplyNums = num1 * num2;
-            return multiplyNums;
+            return num1 * num2;
         }
 
-        public static int divideNums(int num1, int num2)
+        public static int Divide(int num1, int num2)
         {
-            int divideNums = num1 / num2;
-            return divideNums;
+            return num1 / num2;
         }
 
-        public static int modulusNums(int num1, int num2)
+        public static int Modulus(int num1, int num2)
         {
-            int modulusNums = num1 / num2;
-            return modulusNums;
+        return num1 / num2;
+        }
+
+        //Bonus
+        public static int AddAllNums(params int[] nums)
+        {
+            int sum = 0;
+            foreach(var number in nums)
+            {
+                sum = sum + number;
+            }
+
+            return sum;
+            
         }
     }
 }
